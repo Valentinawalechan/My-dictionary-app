8a22420717b0e2d850b54c4833a2f76b1948e925
Valentinawalechan <vale.disa86@gmail.com>
2021-06-15 22:37:33 +0100

    update commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Dictionary</h1>
+      <h1>Dictionary App</h1>
       </header>
     </div>
   );
