7105f1ef8ebb0cb67fbd4919e25a04f35f48bd30
Valentinawalechan <vale.disa86@gmail.com>
2021-06-16 09:42:11 +0100

    created new Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Dictionary App</h1>
+        LOGO
       </header>
+       <main>
+      <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+          Coded by Valentina Di Santo
+        </small>
+      </footer>
     </div>
   );
 }
